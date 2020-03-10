fa6eb1d99591713aaf16d9551056d7586bf689c1
Glitch (hello-express) <none>
2020-03-10 02:17:31 +0000

    🐊🚌 Checkpoint ./server.js:12715998/900 ./signaling.js:12715998/533 ./public/driver.js:12715998/401

--- public/driver.js
+++ public/driver.js
@@ -1,2 +1,7 @@
 console.log("driver");
 
+var socket = new WebSocket("signaling", "protocolOne");
+
+socket.onopen = function (event) {
+  socket.send("Hello from driver");
+};
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const { createServer } = require("http");
 const express = require("express");
-const app = express();
 const WebSocket = require("ws");
 
-// make all the files in 'public' available
+// Configure express for serving files
+const app = express();
+app.use(express.json({ extended: false }));
 app.use(express.static("public"));
+app.use("/signaling", require("./signaling.js"));
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/driver.html");
 });
@@ -12,6 +14,17 @@ app.get("/robot", (request, response) => {
   response.sendFile(__dirname + "/views/robot.html");
 });
 
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+// Launch express server
+const server = createServer(app);
+server.listen(process.env.PORT, () => {
+  console.info(`Server running on port: ${process.env.PORT}`);
+});
+
+// Launch websocket server
+const webSocketServer = new WebSocket.Server({ server });
+webSocketServer.on("connection", (webSocket) => {
+
+    console.info("Total connected clients:", webSocketServer.clients.size);
+
+    app.locals.clients = webSocketServer.clients;
 });
