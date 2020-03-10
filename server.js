f05e9621dbd5b636e49fca71d9a2ad64ec4b60d3
Glitch (hello-express) <none>
2020-03-10 02:41:40 +0000

    🛀🦀 Checkpoint ./server.js:12715998/3970 ./views/robot.html:12715998/63

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const WebSocket = require("ws");
 const app = express();
 app.use(express.json({ extended: false }));
 app.use(express.static("public"));
-app.use("/signaling", require("./signaling.js"));
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/driver.html");
 });
@@ -22,19 +21,17 @@ server.listen(process.env.PORT, () => {
 
 // Launch websocket server
 const webSocketServer = new WebSocket.Server({ server });
-webSocketServer.on("connection", (socket) => {
+webSocketServer.on("connection", socket => {
   console.info("Total connected clients:", webSocketServer.clients.size);
   app.locals.clients = webSocketServer.clients;
 
-  socket.on("message", (message) => {
-    // Send all messages to all other clients
-    for (var i = 0; i < webSocketServer.clients.length; i++) {
-      var c = webSocketServer.clients[i];
-      if (socket != c) {
-        c.send(message);
+  // Send all messages to all other clients
+  socket.on("message", message => {
+    webSocketServer.clients.forEach(client => {
+      if (client != socket && client.readyState === WebSocket.OPEN) {
+        client.send(message);
       }
-    }
+    });
     console.log(message);
   });
-
 });
